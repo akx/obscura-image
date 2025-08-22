6b479fc1498fd19c31fd4209f32c1e6a17894605
Aarni Koskela <akx@iki.fi>
2025-08-22 10:03:27 +0300

    Add manually written TypeScript types

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,37 @@ use png::encode_png;
 use typ::{DecodeResult, Image, Output};
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(js_name = "decodeTiff")]
+// TODO: it would be nicer to generate these automatically, with e.g.
+//       `tsify`, but I couldn't get it to work with `serde_bytes`
+//       and `wasm-bindgen` so we'd still keep emitting an Uint8Array...
+#[wasm_bindgen(typescript_custom_section)]
+const TS_CUSTOM_TYPES: &'static str = r#"
+export interface ImageMetadata {
+  image_index: number;
+  width: number;
+  height: number;
+  color_type: string;
+  bit_depth: number;
+}
+
+export interface Image {
+  png_data: Uint8Array;
+  metadata: ImageMetadata;
+}
+
+export interface ImageDecodeError {
+  image_index: number;
+  message: string;
+}
+
+export interface Output {
+  images: Image[];
+  errors: ImageDecodeError[];
+  total_images: number;
+}
+"#;
+
+#[wasm_bindgen(js_name = "decodeTiff", unchecked_return_type = "Output")]
 pub fn js_decode_tiff(
     #[wasm_bindgen(js_name = "tiffData")] tiff_data: &[u8],
 ) -> std::result::Result<JsValue, JsValue> {
